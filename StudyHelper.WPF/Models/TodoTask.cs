4e87a0bd23fb09be36480b1a3560bb9220df2340
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-03-30 11:29:11 +0200

    Restructure project in more modular way and implement TodoTask model

---
+++ StudyHelper.WPF/Models/TodoTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyHelper.WPF.Models
+{
+    public class TodoTask
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly? Deadline { get; set; }
+
+
+        public TodoTask(Guid id, string title, string description, DateOnly deadline)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Deadline = deadline;
+        }
+    }
+}
